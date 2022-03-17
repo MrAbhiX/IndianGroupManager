202f47e304eb51fb8522809308798c6d2e27dc44
ABHISHEK THAKUR <99659192+MrAbhiX@users.noreply.github.com>
2022-03-18 00:57:05 +0530

    Update afk.py

--- TeamIndia/modules/afk.py
+++ TeamIndia/modules/afk.py
@@ -6,7 +6,7 @@ from .disable import (
     DisableAbleMessageHandler,
 )
 from .sql import afk_sql as sql
-from .broadcast import get_user_id
+from .users import get_user_id
 from telegram import MessageEntity, Update
 from telegram.error import BadRequest
 from telegram.ext import CallbackContext, Filters, MessageHandler, run_async
