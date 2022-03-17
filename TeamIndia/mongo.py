326a591bcc5d3a025d5ea85f78e5ddeafee1638e
ABHISHEK THAKUR <99659192+MrAbhiX@users.noreply.github.com>
2022-03-18 02:14:41 +0530

    Update mongo.py

--- TeamIndia/mongo.py
+++ TeamIndia/mongo.py
@@ -10,14 +10,14 @@ from TeamIndia.conf import get_int_key, get_str_key
 
 MONGO_PORT = get_int_key("27017")
 MONGO_DB_URI = get_str_key("MONGO_DB_URI")
-MONGO_DB = "AnosVoldigoad"
+MONGO_DB = "TeamIndia"
 
 
 client = MongoClient()
 client = MongoClient(MONGO_DB_URI, MONGO_PORT)[MONGO_DB]
 motor = motor_asyncio.AsyncIOMotorClient(MONGO_DB_URI, MONGO_PORT)
 db = motor[MONGO_DB]
-db = client["HaritaRobot"]
+db = client["TeamIndianRobot"]
 try:
     asyncio.get_event_loop().run_until_complete(motor.server_info())
 except ServerSelectionTimeoutError:
